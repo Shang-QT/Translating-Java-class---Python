16a92a465e42d678232e95c87915c9924b4c4c9f
Lloyd Justin <felecildaj@gmail.com>
2025-10-28 21:25:26 +0800

    fgn

---
+++ Activity.py
@@ -0,0 +1,24 @@
+class Dog:
+    def __init__(itself, name, age):
+        itself.name = name
+        itself.age = age
+    
+    def bark(isself):
+        print("Woof! Woof!")
+    
+    def celebrateBirthday(itself):
+        itself.age += 1
+        print(f"Happy birthday! {itself.name} is now {itself.age} years old!")
+    
+    def getInfo(itself):
+        return f"Dog Name: {itself.name}, Age: {itself.age}"
+
+if __name__ == "__main__":
+    name = input("Enter the dog's name: ")
+    age = int(input("Enter the dog's age: "))
+
+    mydog = Dog(name, age)
+    print(mydog.getInfo())
+    mydog.bark()
+    mydog.celebrateBirthday()
+    print(mydog.getInfo())
